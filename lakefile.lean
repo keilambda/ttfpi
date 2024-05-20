b60c4a72ceeef29d6e8c4669bf9910f0349112e1
thelissimus <thelissimus@tuta.io>
2024-05-20 21:57:25 +0500

    chore: fix repo name

--- lakefile.lean
+++ lakefile.lean
@@ -1,14 +1,14 @@
 import Lake
 open Lake DSL
 
-package «ttfp» where
+package «ttfpi» where
   -- add package configuration options here
 
-lean_lib «Ttfp» where
+lean_lib «TTFPI» where
   -- add library configuration options here
 
 require batteries from git "https://github.com/leanprover-community/batteries" @ "main"
 
 @[default_target]
-lean_exe «ttfp» where
+lean_exe «ttfpi» where
   root := `Main
