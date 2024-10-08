48d1f66bbd6c1e5a03c30f2c2c81e09c696d1046
thelissimus <thelissimus@tuta.io>
2024-10-09 04:06:14 +0500

    fix(UntypedExamples): brush up on new function names

--- TTFPI/UntypedExamples.lean
+++ TTFPI/UntypedExamples.lean
@@ -5,27 +5,22 @@ import Batteries.Data.RBMap.Lemmas
 
 open Λ
 
-def I : Λ := lam "x" ↦ "x"
-def I2 : Λ := lam "y" ↦ "y"
+def I : Λ := lam "y" ↦ "y"
 
 open Batteries in
-theorem IdentityRenaming : Renaming I I2 := by
-  have foo : ¬ ("y" ∈ FV "x") := by
-    simp [FV, Membership.mem, RBSet.Mem, RBSet.MemP, RBNode.MemP, RBNode.Any, compare, compareOfLessAndEq]
-  have bar : ¬ (hasBindingVar "y" "x") := by
-    simp [hasBindingVar]
-  exact (.rename foo bar)
+theorem renamingI : Renaming Combinators.I I := by
+  constructor
+  · simp [FV, (· ∈ ·), RBSet.Mem, RBSet.MemP, RBNode.MemP, compare, compareOfLessAndEq]
+  · simp
+
+example : AlphaEq Combinators.I I :=
+  AlphaEq.rename renamingI
 
 def x2y := lam "x" ↦ "y"
 #eval x2y.rename "y" "z"
 
--- example (hr : Renaming Rex (Rex.rename "y" "z")) : "z" ∈ FV (Rex.rename "y" "z") := by
---   simp [FV, (·∈·), Batteries.RBSet.Mem, Batteries.RBSet.MemP, Batteries.RBNode.MemP, Batteries.RBNode.Any, compare, compareOfLessAndEq]
---   cases hr with
---   | rename _ => sorry
-
 def ex : Λ := lam "x" ↦ ("x" :$ "y")
-#eval ex.subst "y" I |> toString
+#eval ex.subst' "y" I |> toString
 #eval ex["y" := I] |> toString
 #eval (I :$ "x").reduceβ |> toString
 #eval →β(I :$ "x")
@@ -42,6 +37,6 @@ def ex : Λ := lam "x" ↦ ("x" :$ "y")
 def M := (lam "x" ↦ "y") :$ "z"
 #eval M.toString
 
-#eval Λ.subst M "x" "y" |> toString
-#eval Λ.subst M "z" "y" |> toString
+#eval Λ.subst' M "x" "y" |> toString
+#eval Λ.subst' M "z" "y" |> toString
 #eval Λ.reduceβ M
