f63de36c8b0eaa5cb0e6d9ee8cc9e7a08aeb41d6
thelissimus <thelissimus@tuta.io>
2024-11-02 14:38:51 +0500

    refactor(Basic): get rid of redundant notations

--- TTFPI/Basic.lean
+++ TTFPI/Basic.lean
@@ -2,9 +2,6 @@ import Mathlib.Data.Multiset.Basic
 
 universe u
 
-@[inherit_doc] infix:50 " ≡ " => Eq
-macro_rules | `($x ≡ $y) => `(binrel% Eq $x $y)
-
 unsafe instance {α : Type u} [ToString α] : ToString (Multiset α) where
   toString s := toString (s.unquot.map toString)
 
