71657b20c423668f58d32e32786bc39777c7a61e
thelissimus <thelissimus@tuta.io>
2024-11-02 22:00:08 +0500

    add(SimplyTyped): definition of a simply typed lambda term

--- TTFPI/SimplyTyped.lean
+++ TTFPI/SimplyTyped.lean
@@ -19,3 +19,12 @@ protected def toString : Typ → String
 instance : ToString Typ := ⟨Typ.toString⟩
 
 end Typ
+
+-- 2.4.1: Pre-typed λ-terms
+inductive Term where
+| var (name : Name)
+| app (fn : Term) (arg : Term)
+| abs (param : Name) (type : Typ) (body : Term)
+deriving Repr
+
+instance : Coe Name Term := ⟨Term.var⟩
