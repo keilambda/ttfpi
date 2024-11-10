ac584f39c6e912da7ccbeb96992e8b5a2f061e30
thelissimus <thelissimus@tuta.io>
2024-11-11 00:53:23 +0500

    chore: add `doc-gen4` for documentation generation

--- lakefile.lean
+++ lakefile.lean
@@ -13,3 +13,6 @@ require batteries from git "https://github.com/leanprover-community/batteries" @
 require aesop from git "https://github.com/leanprover-community/aesop" @ "v4.13.0"
 require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.13.0"
 require calcify from git "https://github.com/nomeata/lean-calcify"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "v4.13.0"
