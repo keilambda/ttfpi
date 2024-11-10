6cbf65f416383c6167160c8bda0053c879cf3d80
thelissimus <thelissimus@tuta.io>
2024-11-11 03:07:55 +0500

    add(SecondOrder): `Kind`, `Typ` and `Term` definitions

--- TTFPI/SecondOrder.lean
+++ TTFPI/SecondOrder.lean
@@ -1,2 +1,26 @@
+import TTFPI.Basic
+
 namespace SecondOrder
+
+inductive Kind where
+| star
+deriving Repr, DecidableEq
+
+notation " ∗ " => Kind.star
+
+inductive Typ where
+| var (name : Name)
+| arrow (dom : Typ) (codom : Typ)
+| pi (param : Name) (kind : Kind) (body : Typ)
+deriving Repr, DecidableEq
+
+-- 3.4.1: Second order pre-typed λ-terms
+inductive Term where
+| var (name : Name)
+| app (fn : Term) (arg : Term)
+| tapp (fn : Term) (arg : Typ)
+| abs (param : Name) (type : Typ) (body : Term)
+| tabs (param : Name) (kind : Kind) (body : Term)
+deriving Repr, DecidableEq
+
 end SecondOrder
