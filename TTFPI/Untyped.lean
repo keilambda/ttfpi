0b08dcd01ca429b6e6c3a09d85dc3b35e539c785
thelissimus <thelissimus@tuta.io>
2024-11-10 18:52:42 +0500

    add(Untyped): modulo alpha equivalence

--- TTFPI/Untyped.lean
+++ TTFPI/Untyped.lean
@@ -318,6 +318,13 @@ lemma subst_sequence (h : x ≠ y) (hxm : x ∉ L.FV) : M[x := N][y := L] = M[y 
       · simp_all [subst]
     · sorry
 
+-- 1.7.1: modulo α-equivalence
+lemma modulo_alpha_eq_app (hmp : M =α P) (hnq : N =α Q) : M ∙ P =α N ∙ Q := sorry
+
+lemma modulo_alpha_eq_abs (h : M =α N) : abs x M =α abs x N := sorry
+
+lemma modulo_alpha_eq_subst (hmp : M =α P) (hnq : N =α Q) : M[x := P] =α N[x := Q] := sorry
+
 -- 1.8.1: One-step β-reduction; →β
 def reduceβ (t : Λ) : Λ :=
   match t with
