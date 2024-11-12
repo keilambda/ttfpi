9f5fa4203529136a489918af619f2a57487a378f
thelissimus <thelissimus@tuta.io>
2024-11-12 23:59:50 +0500

    refactor(SecondOrder): naming of type params

--- TTFPI/SecondOrder.lean
+++ TTFPI/SecondOrder.lean
@@ -13,7 +13,7 @@ instance : ToString Kind := ⟨fun k => match k with | .star => "∗"⟩
 inductive Typ where
 | var (name : Name)
 | arrow (dom : Typ) (codom : Typ)
-| pi (param : Name) (kind : Kind) (body : Typ)
+| pi (binder : Name) (kind : Kind) (body : Typ)
 deriving Repr, DecidableEq
 
 namespace Typ
@@ -21,7 +21,7 @@ namespace Typ
 protected def toString : Typ → String
 | var α => α
 | arrow dom codom => s!"{dom.toString} → {codom.toString}"
-| pi param kind body => s!"Π {param} : {kind}. {body.toString}"
+| pi binder kind body => s!"Π {binder} : {kind}. {body.toString}"
 
 instance : ToString Typ := ⟨Typ.toString⟩
 
@@ -29,7 +29,7 @@ instance : Coe Name Typ := ⟨var⟩
 
 infixr:20 " ⇒ " => arrow
 
-notation "Π" param ":" kind "," body => pi param kind body
+notation "Π" binder ":" kind "," body => pi binder kind body
 
 end Typ
 
@@ -39,7 +39,7 @@ inductive Term where
 | app (fn : Term) (arg : Term)
 | tapp (fn : Term) (arg : Typ)
 | abs (param : Name) (type : Typ) (body : Term)
-| tabs (param : Name) (kind : Kind) (body : Term)
+| tabs (binder : Name) (kind : Kind) (body : Term)
 deriving Repr, DecidableEq
 
 namespace Term
@@ -49,7 +49,7 @@ protected def toString : Term → String
 | app fn arg => s!"({fn.toString} {arg.toString})"
 | tapp fn arg => s!"({fn.toString} [{arg.toString}])"
 | abs param type body => s!"(λ{param} : {type}. {body.toString})"
-| tabs param kind body => s!"(Λ{param} : {kind}. {body.toString})"
+| tabs binder kind body => s!"(Λ{binder} : {kind}. {body.toString})"
 
 instance : ToString Term := ⟨Term.toString⟩
 
@@ -59,7 +59,7 @@ infixl:100 " ∙ " => app
 
 infixl:100 " ∙ₜ " => tapp
 
-notation "Λ" param ":" kind "," body => tabs param kind body
+notation "Λ" binder ":" kind "," body => tabs binder kind body
 
 end Term
 end SecondOrder
