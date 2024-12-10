04838f5a1e47a685980c6f8119b30569a1838a66
thelissimus <thelissimus@tuta.io>
2024-12-11 00:26:14 +0500

    add(Basic): Finset ToString unsafe instance

--- TTFPI/Basic.lean
+++ TTFPI/Basic.lean
@@ -1,3 +1,4 @@
+import Mathlib.Data.Finset.Basic
 import Mathlib.Data.Multiset.Basic
 
 universe u
@@ -6,3 +7,6 @@ abbrev Name := String
 
 unsafe instance {α : Type u} [ToString α] : ToString (Multiset α) where
   toString s := toString (s.unquot.map toString)
+
+unsafe instance {α : Type u} [ToString α] : ToString (Finset α) where
+  toString s := toString s.1
